f372dc249a4667bbd5c5197183e99c5c86f5902f
Fabriciomdec <fabriciomdec@gmail.com>
2025-05-22 16:27:44 -0300

    chore: rename attendance tabble

--- src/infra/database/init-db.ts
+++ src/infra/database/init-db.ts
@@ -2,7 +2,7 @@ import { sql } from './database.provider';
 
 export async function createTables() {
   await sql`
-    CREATE TABLE IF NOT EXISTS attendance (
+    CREATE TABLE IF NOT EXISTS attendances (
       id UUID PRIMARY KEY,
       description TEXT NOT NULL,
       name TEXT NOT NULL,
